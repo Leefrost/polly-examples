a07fa50e2a66ab86349de4214e0d6b16698c2428
Leefrost <evertodante@gmail.com>
2021-04-16 21:29:19 +0300

    dev (docker) Prepare docker structure

--- src/CallingService/Startup.cs
+++ src/CallingService/Startup.cs
@@ -27,10 +27,13 @@ namespace CallingService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "CallingService", Version = "v1"});
             });
+            
+            services.AddHttpClient()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
